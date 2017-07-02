f452cf22ef32c4d98d1f4d7fd892f752d1d04f18
ujwalp15 <ujwalp09@gmail.com>
2017-07-02 23:45:51 +0530

    LR: use matplotlib to plot the graph and visualize it

--- lreg.py
+++ lreg.py
@@ -1,5 +1,6 @@
 import pandas as pa
 from sklearn import linear_model
+import matplotlib.pyplot as plot
 
 data_set = pa.read_fwf('age_bp.txt')
 x_values = data_set[['Age']]
@@ -7,3 +8,7 @@ y_values = data_set[['BloodPressure']]
 
 bp_prediction = linear_model.LinearRegression()
 bp_prediction.fit(x_values, y_values)
+
+plot.scatter(x_values, y_values)
+plot.plot(x_values, bp_prediction.predict(x_values))
+plot.show()
